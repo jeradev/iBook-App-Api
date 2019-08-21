56f1d184085310131e3f03f9979d0880008a6aa7
razvanjega <?/2k18Gh96/?>
2019-08-21 09:58:11 +0300

    name queries

--- src/main/java/sol/jeradev/iBookAppApi/repository/CustomerRepository.java
+++ src/main/java/sol/jeradev/iBookAppApi/repository/CustomerRepository.java
@@ -3,11 +3,16 @@ package sol.jeradev.iBookAppApi.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sol.jeradev.iBookAppApi.domain.Customer;
 
 //Long is wrapper for primitive type long
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     //queries derived from method names
     Page<Customer> findByFirstNameContainingAndPhone(String partialName, String Phone, Pageable pageable);
+
+    @Query(value = "SELECT * FROM customer WHERE name LIKE '%?1'", nativeQuery = true)
+    Page<Customer>findByPartialAndFirstNameAndLastNameAndPhone (String partialFirstName, String partialLastName, String phone, Pageable pageable);
+
 
 }
